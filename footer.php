d87e4e3565eb7ddcd8185b027b07fe0445223eed
Erin Skidds <dudethatserin@outlook.com>
2021-11-04 18:10:57 -0400

    Initial Commit
    This is how my website is designed and created. I am sharing this so you can look at everything I use.

---
+++ footer.php
@@ -0,0 +1,35 @@
+</section>
+<footer>
+    <p>&copy; 2021 ErinSkidds.com - Website Coded & Created By Erin Skidds using JavaScript, PHP, <a href="https://validator.w3.org/nu/?doc=https%3A%2F%2Ferinskidds.com%2F" target="_blank" title="HTML5 Valid!">HTML5</a> &amp; <a href="https://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2Ferinskidds.com&profile=css3svg&usermedium=all&warning=1&vextwarning=&lang=en" target="_blank" title="CSS3 Valid!">CSS3</a>. It is <a href="https://search.google.com/test/mobile-friendly/result?id=fb15szBPie2K2zCH9QWr6g" target="_blank" title="Mobile Friendly!">Mobile Friendly</a>.</p>
+</footer>
+</div>
+
+    <script>
+    grecaptcha.ready(function() {
+        grecaptcha.execute('6LefRw4dAAAAANexYiV4MxbYODkg3eFkMDBfMIBT', {action: 'submit'}).then(function(token) {
+            var recaptchaResponse = document.getElementById('recaptchaResponse');
+            recaptchaResponse.value = token;
+        });
+    });
+    $(document).on('click', '[data-toggle="lightbox"]', function(event) {
+                event.preventDefault();
+                $(this).ekkoLightbox();
+            });
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+  </script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,136 @@
+<?php include('./header.php'); ?>
+<!-- START OF MAIN PAGE -->
+
+<section>
+    <img src="https://erinskidds.com/img/bluegreenbg_me.jpg" alt="Photo of Erin Skidds" title="Picture of me." id="imgMe" />
+    <h1>Erin Skidds</h1>
+    <table class="top">
+        <tr>
+            <td class="first"><img src="https://erinskidds.com/img/phone.png" alt="phone icon" id="phone" /></td>
+            <td class="second">Not given publicly. If you want to reach me, please email me or fill out the <a href="https://erinskidds.com/contact.php" target="_blank">contact form</a>.</td>
+        </tr>
+        <tr>
+            <td class="first"><img src="https://erinskidds.com/img/mail.png" alt="mail icon" id="mail" /></td>
+            <td class="second">erin.skidds@gmail.com</td>
+        </tr>
+        <tr>
+            <td class="first"><img src="https://erinskidds.com/img/locate.png" alt="location icon" id="locate" /></td>
+            <td class="second">Tampa, FL</td>
+        </tr>
+    </table>
+
+    <br /><p>Seeking an Information Technology job in a dynamic environment where my passion for customer service and selling strengths make me a key member of the IT team. Driven to meet and exceed performance expectations as well as fix any and all issues that come my way.</p>
+
+    <img src="https://erinskidds.com/img/h1side.png" alt="arrow" id="edu">
+    <h2>EDUCATION</h2>
+    <p><strong>FSCJ</strong>, The Florida State College at Jacksonville, Jacksonville, FL<br />
+    Bachelor of IT Management, Department of Information Technology</p>
+
+    <img src="https://erinskidds.com/img/h1side.png" alt="arrow" id="skills">
+    <h2>SKILLS</h2>
+    <p>HTML, CSS, JavaScript, PHP, Node.js, SQL, Git, MySQL, Command Line, WordPress, IP.Board, Xenforo, Discord, Linux, Windows, MacOS, iOS, and Android.</p>
+
+    <img src="https://erinskidds.com/img/h1side.png" alt="arrow" id="arrow">
+    <h2>EXPERIENCE</h2>
+    <table class="experience">
+        <tr>
+            <td class="name">KeyBank National Association</td>
+            <td class="locale">Tampa, FL</td>
+            <td class="date">10/2020-Present</td>
+        </tr>
+    </table>
+
+    <p class="title">Commercial Client Representative</p>
+
+    <ul><li>Received calls from Commercial Clients like Aspen Dental and assisted them with KeyNavigator, KeyCapture, and other Key Related services as well as their checking, savings, and credit card accounts.</li></ul>
+
+    <table class="about2">
+        <tr>
+            <td class="name">Inside Response, LLC</td>
+            <td class="locale">Jacksonville, FL</td>
+            <td class="date">07/2020-10/2020</td>
+        </tr>
+    </table>
+
+    <p class="title">Customer Service Associate</p>
+
+     <ul><li>Received calls from people over the age of 75 regarding their life insurance, Medicare, and Medicaid and referred them to licensed agents that sold them these services.</li></ul>
+
+    <table class="about3">
+        <tr>
+            <td class="name">Availity, LLC</td>
+            <td class="locale">Jacksonville, FL</td>
+            <td class="date">09/2018-05/2019</td>
+        </tr>
+    </table>
+
+    <p class="title">Client Services Representative</p>
+
+    <ul><li>Received calls from medical offices and assisted them with using our client portal.</li></ul>
+
+    <table class="about4">
+        <tr>
+            <td class="name">Best Buy</td>
+            <td class="locale">Jacksonville, FL</td>
+            <td class="date">07/2015-09/2018</td>
+        </tr>
+    </table>
+    <p class="title">Sales Associate & Customer Service Representative</p>
+
+        <ul><li>Started on the sales floor selling phones, tablets, laptops, and accessories.</li><li>Excelled at exceeding sales goals every month.</li><li>Ended in Customer Service, where I sold Total Tech Support over the phone as well as answered customer's questions.</li></ul>
+
+    <img src="https://erinskidds.com/img/h1side.png" alt="arrow" id="projects">
+    <h2>PROJECTS</h2>
+
+    <a href="https://erinskidds.com/img/ch.png" class="img" data-lightbox="lightbox" data-title="CodingHelp[] - a Helpful Coding, Subreddit, Website, and Discord Server. Website is located here: https://codinghelp.site"><img id="side" src="https://erinskidds.com/img/ch.png" alt="" /></a>
+    <table class="ch">
+        <tr>
+            <td class="name">CodingHelp</td>
+            <td class="date"><a href="https://codinghelp.site" target="_blank" title="codinghelp[]">https://codinghelp.site</a></td>
+        </tr>
+    </table>
+    <p class="title"><a href="https://codinghelp.site/" target="_blank">Website</a>, <a href="https://reddit.com/r/CodingHelp" target="_blank">Subreddit with over 41,000 members</a>, <a href="https://codinghelp.site/discord" target="_blank">Discord Server with over 1,800 members</a>.</p>
+    <ul>
+        <li>I am the owner and founder of CodingHelp, a community dedicated to helping new people learn how to code.</li>
+    </ul><br /><br />
+
+    <a href="https://erinskidds.com/img/bot.jpg" class="img" data-lightbox="bot" data-title="No image because this is a discord bot, if you want to learn more, you can go here: https://erinskidds.com/sakura.php"><img id="side" src="https://erinskidds.com/img/bot.jpg" alt="" /></a>
+    <table class="sm">
+        <tr>
+            <td class="name">Sakura Moon</td>
+            <td class="date"><a href="https://erinskidds.com/sakura.php" target="_blank" title="Sakura Moon Discord Bot">Sakura Moon Discord Bot</a></td>
+        </tr>
+    </table>
+    <p class="title">Discord Bot that I created and shared for anyone to use.</p>
+    <ul>
+        <li>This development took me almost two years to do. It is running <a href="https://discord.js.org/#/" target="_blank" title="Discord.js - a Node.js Module">Discord.js</a> which is a <a href="https://nodejs.org/" target="_blank" title="Node.js - a JavaScript runtime built on Chrome's V8 JavaScript Engine">Node.js</a> module that connects to <a href="https://discord.com" target="_blank" title="Discord - A social media platform.">Discord</a> and allows you to create automated robots ("bots") for the platform.</li>
+        <li>This bot has a Challenge System, Suggestions System, Thanks System, Points System, Moderation System, and some just fun random commands. If you want to see everything it can do and see what each of these system are, you can read more <a href="https://erinskidds.com/sakura.php" target="_blank" title="Sakura Moon - Discord Bot">here</a>.</li>
+    </ul>
+
+    <a href="https://erinskidds.com/img/tbs.png" class="img" data-lightbox="tbs" data-title="The Tech Blogger's Space located at https://techblogger.space"><img id="side" src="https://erinskidds.com/img/tbs.png" alt="" /></a>
+    <table class="tbs">
+        <tr>
+            <td class="name">The Tech Blogger's Space</td>
+            <td class="date"><a href="https://techblogger.space" target="_blank" title="The Tech Blogger's Space ('TBS')">https://techblogger.space</a></td>
+        </tr>
+    </table>
+    <p class="title">Blog running on <a href="https://wordpress.com/" target="_blank" title="WordPress - a website platform built in PHP, JavaScript and MySQL">WordPress</a></p>
+    <ul>
+        <li>The Tech Blogger's Space ("TBS") is where I write out all of my opinions on the applications I have tested, devices being announced, and anything I feel needs a shout out. I have been blogging since I started coding when I was 13/14 years old and this is my latest blog. You can find topics on Android, iPhons, Apple, Google, Microsoft, and other applications.</li>
+    </ul>
+
+    <a href="https://erinskidds.com/img/TA360.png" class="img" data-lightbox="ta" data-title="TravelAgent360 - Travel Agent Webste located at https://travelagent360.us"><img id="side" src="https://erinskidds.com/img/TA360.png" alt="TravelAgent360 - Travel Agent Webste" /></a>
+    <table class="ta360">
+        <tr>
+            <td class="name">Travel Agent 360</td>
+            <td class="date"><a href="https://travelagent360.us/" target="_blank" title="Travel Agent 360 - A Travel Agent Website">https://travelagent360.us/</a></td>
+        </tr>
+    </table>
+    <p class="title">Website Powered by <a href="https://wordpress.com/" target="_blank" title="WordPress - a website platform built in PHP, JavaScript and MySQL.">WordPress</a> for Travel Agents.</p>
+    <ul>
+        <li>Andrew Appleton (CEO and Founder) reached out to me to get his website maintained by me. He had the theme partially set up, but it was a mess, and I helped him get it to the point where it is today.I had to code a lot of different scripts and pages from scratch and change a few different items on the website to match what he wanted. It was a unique experience that challenged me.</li>
+    </ul>
+
+
+<!-- END OF MAIN PAGE/INCLUDING FOOTER -->
+<?php include('./footer.php'); ?>
