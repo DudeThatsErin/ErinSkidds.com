00272b747dec84335ad24bc314a096507712b395
Erin Skidds <dudethatserin@outlook.com>
2021-11-04 18:13:08 -0400

    Added link to GitHub in footer.

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 </section>
 <footer>
-    <p>&copy; 2021 ErinSkidds.com - Website Coded & Created By Erin Skidds using JavaScript, PHP, <a href="https://validator.w3.org/nu/?doc=https%3A%2F%2Ferinskidds.com%2F" target="_blank" title="HTML5 Valid!">HTML5</a> &amp; <a href="https://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2Ferinskidds.com&profile=css3svg&usermedium=all&warning=1&vextwarning=&lang=en" target="_blank" title="CSS3 Valid!">CSS3</a>. It is <a href="https://search.google.com/test/mobile-friendly/result?id=fb15szBPie2K2zCH9QWr6g" target="_blank" title="Mobile Friendly!">Mobile Friendly</a>.</p>
+    <p>&copy; 2021 ErinSkidds.com - Website Coded & Created By Erin Skidds using JavaScript, PHP, <a href="https://validator.w3.org/nu/?doc=https%3A%2F%2Ferinskidds.com%2F" target="_blank" title="HTML5 Valid!">HTML5</a> &amp; <a href="https://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2Ferinskidds.com&profile=css3svg&usermedium=all&warning=1&vextwarning=&lang=en" target="_blank" title="CSS3 Valid!">CSS3</a>. It is <a href="https://search.google.com/test/mobile-friendly/result?id=fb15szBPie2K2zCH9QWr6g" target="_blank" title="Mobile Friendly!">Mobile Friendly</a>. Follow updates on <a href="https://github.com/DudeThatsErin/ErinSkidds.com" tile="ErinSkidds.com on GitHub">GitHub</a>.</p>
 </footer>
 </div>
 
