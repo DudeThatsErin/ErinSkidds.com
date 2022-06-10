42bc666a13f0b858cee8e94e882ec34945802b4d
Erin Skidds <2681022+DudeThatsErin@users.noreply.github.com>
2022-06-10 14:30:05 -0400

    testing swiper

--- angular/test1/src/app/app.component.ts
+++ angular/test1/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+// import Swiper core and required modules
+import SwiperCore, { Virtual } from 'swiper';
+
+// install Swiper modules
+SwiperCore.use([Virtual]);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'test1';
+  title = 'Erin Skidds';
+
+  // Create array with 1000 slides
+  slides = Array.from({ length: 1000 }).map(
+    (el, index) => `Slide ${index + 1}`
+  );
 }
